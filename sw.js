77f727c1d8a730f3da0c929645cf7f655621973b
secfcosantana-cell <secfcosantana@gmail.com>
2026-09-22 13:40:14 -0600

    Actualizar caché para mostrar logo escolar

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='portal-escolar-v6';
+const CACHE='portal-escolar-v7';
 const ASSETS=['./','./index.html','./styles.css','./app-local.js','./portal-data.json','./manifest.webmanifest','./assets/logo.svg','./assets/cell.svg','./assets/microscope.svg','./assets/bacteria.svg','./assets/dna.svg','./icon-192.png','./icon-512.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
