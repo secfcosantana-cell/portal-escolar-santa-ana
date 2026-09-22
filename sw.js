233cc65f578a06a0365047d2d6a6cfc3d49dd414
secfcosantana-cell <secfcosantana@gmail.com>
2026-09-22 13:14:58 -0600

    Cache PNG mobile icon

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE='portal-escolar-v6';
-const ASSETS=['./','./index.html','./styles.css','./app-local.js','./portal-data.json','./manifest.webmanifest','./logo-escuela.svg','./cell.svg','./microscope.svg','./bacteria.svg','./dna.svg','./icon-192.svg','./icon-512.svg'];
+const ASSETS=['./','./index.html','./styles.css','./app-local.js','./portal-data.json','./manifest.webmanifest','./logo-escuela.svg','./cell.svg','./microscope.svg','./bacteria.svg','./dna.svg','./icon-192.png','./icon-512.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{const u=new URL(e.request.url);if(u.origin!==location.origin)return;e.respondWith(caches.match(e.request).then(c=>c||fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(x=>x.put(e.request,copy));return r}).catch(()=>caches.match('./index.html'))));});
